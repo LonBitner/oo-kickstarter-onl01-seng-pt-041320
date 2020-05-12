4df534066880c6288e94fb9dad1faeb0e8edae42
Lon Bitner <lonbitner@gmail.com>
2020-05-12 04:04:45 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,9 +1,9 @@
 class Backer
-  attr_reader :name
+  attr_reader :title
   attr_accessor :backed_projects
 
   def initialize(name)
-    @name = name
+    @title = title
     @backed_projects = []
   end
 

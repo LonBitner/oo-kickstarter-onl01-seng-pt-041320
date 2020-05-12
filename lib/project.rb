a8ec6e3b550d41eefc7c59c7a5f13d1f10dbc229
Lon Bitner <lonbitner@gmail.com>
2020-05-12 04:14:01 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -5,10 +5,9 @@ class Project
     @title = title
     @backers = []
   end
-  
+
   def add_backer(backer)
     @backers << backer
     backer.back_project(self) unless backer.backed_projects.include?(self)
   end
 end
-    
